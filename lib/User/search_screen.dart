827445c20d8348434bd7239e6701c885d07e72ce
unknown <putraulung28@mhs.mdp.ac.id>
2024-07-04 19:07:45 +0700

    perbaikan search screen menambah fitur

---
+++ lib/User/search_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:katalog_motor/detail_screen/PostDetailScreen.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key});
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _searchResults = [];
+
+  Future<void> _searchPosts(String query) async {
+    if (query.isEmpty) {
+      setState(() {
+        _searchResults = [];
+      });
+      return;
+    }
+
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('title', isGreaterThanOrEqualTo: query)
+          .where('title', isLessThanOrEqualTo: query + '\uf8ff')
+          .get();
+
+      List<Map<String, dynamic>> results = [];
+      for (var doc in querySnapshot.docs) {
+        var data = doc.data() as Map<String, dynamic>;
+        data['id'] = doc.id; // Add the document ID to the data
+        results.add(data);
+      }
+
+      setState(() {
+        _searchResults = results;
+      });
+    } catch (e) {
+      print('Error searching posts: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Silahkan cari disini',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    _searchPosts(_searchController.text);
+                  },
+                ),
+              ),
+              onSubmitted: (query) {
+                _searchPosts(query);
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _searchResults.length,
+                itemBuilder: (context, index) {
+                  var data = _searchResults[index];
+                  var postTime = data['time'] as Timestamp?;
+                  var date = postTime?.toDate();
+                  var formattedDate = date != null
+                      ? DateFormat('dd/MM/yyyy HH:mm').format(date)
+                      : 'Unknown date';
+
+                  var title = data['title'] ?? 'No Title';
+                  var imageUrl = data['urlimage'] as String?;
+                  var description = data['descr'] ?? 'No Description';
+                  var postId = data['id'] ?? 'Unknown ID';
+                  var harga = data['harga']?.toString() ?? 'No Harga';
+
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PostDetailScreen(post: {
+                            'id': postId,
+                            'time': data['time'],
+                            'title': title,
+                            'urlimage': imageUrl,
+                            'descr': description,
+                            'harga': harga,
+                          }),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      margin: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (imageUrl != null) Image.network(imageUrl),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        title,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 24,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(
+                                      'Rp.$harga',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  formattedDate,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(description),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
